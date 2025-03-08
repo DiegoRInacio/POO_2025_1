2888b5f640cda1b9820407c3c71a68c447f201e2
DiegoRInacio <diegori.geo@mail.com>
2025-03-07 22:14:05 -0300

    update_07/03/2025

---
+++ Aula 3/Java/Carro.java
@@ -0,0 +1,23 @@
+package Aula 3.Java;
+
+// definindo os atributos
+public class Carro {
+    public String marca;
+    public String modelo;
+    public Integer ano;
+    public String cor;
+    public String placa;
+    public Boolean is_runnig;
+    public Integer velocidade;
+
+    // definindo o método construtor
+    public Carro(String marca, String modelo, Integer ano, String cor, String placa) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.placa = placa;
+        this.is_runnig = false;
+        this.velocidade = 0;
+    }
+}
